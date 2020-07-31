1e05ad591411582618d6ca32638597342449d278
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-07-31 12:51:23 +0200

    updated scraper to use then

--- scraper/index.js
+++ scraper/index.js
@@ -3,25 +3,22 @@ import fetch from 'node-fetch'
 import jsdom from 'jsdom'
 const { JSDOM } = jsdom
 
-const downloadEmojis = async () => {
-  const emojis = []
+const emojis = [];
 
-  const response = await fetch('https://unicode.org/emoji/charts/full-emoji-list.html')
-  const text = await response.text()
-  const document = new JSDOM(text).window.document;
-  const tableRows = document.querySelectorAll('tr');
-  for (const tr of tableRows) {
-    try {
-      emojis.push({
-        emoji: tr.querySelector('.chars').textContent,
-        name: tr.querySelector('.name').textContent
-      })
-    } catch (err) {
+fetch("https://unicode.org/emoji/charts/full-emoji-list.html")
+  .then(response => response.text())
+  .then(text => {
+    const document = new JSDOM(text).window.document;
+    const tableRows = document.querySelectorAll('tr');
+    for (const tr of tableRows) {
+      try {
+        emojis.push({
+          emoji: tr.querySelector('.chars').textContent,
+          name: tr.querySelector('.name').textContent
+        })
+      } catch (err) {
 
+      }
     }
-  }
-
-  fs.writeFile('emojis.json', JSON.stringify(emojis, null, 2), 'utf-8', () => console.log('done'))
-}
-
-downloadEmojis()
+    fs.writeFile('emojis.json', JSON.stringify(emojis, null, 2), 'utf-8', () => console.log('done'))
+  })
