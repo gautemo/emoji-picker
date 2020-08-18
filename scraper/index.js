ea160fa2c645e27b401a492aaa4179ceed022aac
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-08-18 21:24:02 +0200

    updated scraper to another source with categories

--- scraper/index.js
+++ scraper/index.js
@@ -3,22 +3,31 @@ import fetch from 'node-fetch'
 import jsdom from 'jsdom'
 const { JSDOM } = jsdom
 
-const emojis = [];
+const getCategory = async (subpath, category) => {
+  const response = await fetch(`https://emojipedia.org/${subpath}`)
+  const text = await response.text();
+  const document = new JSDOM(text).window.document;
+  const list = document.querySelector('.emoji-list');
+  const emojiCategory = { category, emojis: []}
+  for(const li of list.children){
+    const description = li.children[0].textContent
+    const emoji = li.children[0].children[0].textContent
+    emojiCategory.emojis.push({emoji, description})
+  }
+  return emojiCategory
+}
 
-fetch("https://unicode.org/emoji/charts/full-emoji-list.html")
-  .then(response => response.text())
-  .then(text => {
-    const document = new JSDOM(text).window.document;
-    const tableRows = document.querySelectorAll('tr');
-    for (const tr of tableRows) {
-      try {
-        emojis.push({
-          emoji: tr.querySelector('.chars').textContent,
-          name: tr.querySelector('.name').textContent
-        })
-      } catch (err) {
+const addCategoriesToFile = async () => {
+  const c1 = getCategory('people', 'Smileys & People')
+  const c2 = getCategory('nature', 'Animals & Nature')
+  const c3 = getCategory('food-drink', 'Food & Drink')
+  const c4 = getCategory('activity', 'Activity')
+  const c5 = getCategory('travel-places', 'Travel & Places')
+  const c6 = getCategory('objects', 'Objects')
+  const c7 = getCategory('symbols', 'Symbols')
+  const c8 = getCategory('flags', 'Flags')
+  const emojis = await Promise.all([c1, c2, c3, c4, c5, c6, c7, c8])
+  fs.writeFile('../app/src/assets/emojis.json', JSON.stringify(emojis, null, 2), 'utf-8', () => console.log('done'))
+}
 
-      }
-    }
-    fs.writeFile('../app/src/assets/emojis.json', JSON.stringify(emojis, null, 2), 'utf-8', () => console.log('done'))
-  })
+addCategoriesToFile()
